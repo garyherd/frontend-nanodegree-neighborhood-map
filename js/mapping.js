60f381dfdb9084e23ccf10a38e73c934ff102608
Gary <garyherd@yahoo.com>
2016-10-30 00:44:46 -0500

    feat: Add Foursquare content to infowindows. Add title to hamburger menu

--- js/mapping.js
+++ js/mapping.js
@@ -2,7 +2,7 @@ var map;
 var largeInfoWindow;
 var markers = [];
 var locations = [
-    {name: "Bellagio Apts", location: {lat: 29.888504, lng: -95.5501487}},
+    {name: "Bellagio Apts", location: {lat: 29.888504, lng: -95.54796}},
     {name: "Rudy's BBQ", location: {lat: 29.866844, lng: -95.538356}},
     {name: "George Bush Park", location: {lat: 29.7196437, lng: -95.686953}},
     {name: "Memorial Park", location: {lat: 29.764992, lng: -95.4373088}},
@@ -80,18 +80,24 @@ function populateInfoWindow(marker, infowindow) {
 function getFourSquareInfo(infowindow, successFunc, errorFunc) {
     var self = this;
     var resultStr;
-    var fsquareUrl = "https://api.foursquare!.com/v2/venues/search?ll="
-    + infowindow.marker.getPosition().lat() + "," + infowindow.marker.getPosition().lat()
-    + "&client_id=JE3Z0E1BTGQ5H4KPVTMBBKDFK000WAAAO21FMQIW3ZIV2WBW"
-    + "&client_secret=ZJMZH553AONNR5RFOE5QZOBZBOP0GAOJLWEF2H5OC1IPRCJK&v=20161026";
-
 
     $.ajax({
-        url: fsquareUrl,
+        url: "https://api.foursquare.com/v2/venues/search",
+        data: {
+            ll: infowindow.marker.getPosition().lat().toString() + "," + infowindow.marker.getPosition().lng().toString(),
+            client_id: "JE3Z0E1BTGQ5H4KPVTMBBKDFK000WAAAO21FMQIW3ZIV2WBW",
+            client_secret: "ZJMZH553AONNR5RFOE5QZOBZBOP0GAOJLWEF2H5OC1IPRCJK",
+            v: '20161026'
+        },
         dataType: "json",
-        success: function (data) {
-            console.log(data);
-            // successFunc(infowindow, response);
+        success: function(data) {
+            var venue = data.response.venues[0];
+            var iwData = {
+                name: venue.name,
+                contactInfo: venue.contact,
+                foursquareUrl: "https://foursquare.com/v/" + venue.id.toString()
+            };
+            successFunc(iwData, infowindow);
         },
         error: function (xhr) {
             errorFunc(infowindow);
@@ -99,18 +105,27 @@ function getFourSquareInfo(infowindow, successFunc, errorFunc) {
     });
 }
 
-function createInfoWindowContent(infowindow, response){
-    var resultStr = '<div style="color:black">Title: '+ 'success' + '</div>'
-    + '<div style="color:black">Phone: 555-555-5555</div>'
-    + '<div style="color:black">Web: www.stuff.com</div>'
-    + '<br>'
-    + '<a href="www.foursquare.com">See more at Four Square</a>';
-    // infowindow.setContent(resultStr);
-    console.log(response.statusCode());
+function createInfoWindowContent(iwObj, infowindow){
+    var targetKeys = ["formattedPhone", "twitter", "facebookUsername"]
+    var keyUrls = {formattedPhone: "", twitter: "https://twitter.com/", facebookUsername: "https://facebook.com/"}
+    var resultStr = '<div style="color:black"><b>'+ iwObj.name + '</b></div><br>'
+    $.each(iwObj.contactInfo, function(key, value) {
+        console.log(key, value);
+        if (targetKeys.indexOf(key) > -1) {
+            if (key == "formattedPhone") {
+                resultStr += '<div style="color:black"><b>' + key + '</b>:&nbsp;' + keyUrls[key].toString() + value.toString() + '</div>';
+            } else {
+                resultStr += '<div style="color:black"><b>' + key + ':&nbsp;<a style="color:blue" href="' + keyUrls[key].toString() + value.toString() + '">' + value.toString() + '</a></div>';
+            }
+        };
+    });
+    resultStr += '<br><a style="color:blue" href="' + iwObj.foursquareUrl + '">See more at FourSquare</a>';
+    var replacedPhone = resultStr.replace("formattedPhone", "Phone");
+    var finalResultStr = replacedPhone.replace("facebookUsername", "facebook");
+    infowindow.setContent(finalResultStr);
 }
 
 function createDefaultInfoWindowContent(infowindow) {
-    window.alert("Error function reached");
     infowindow.setContent('<div style="color:black">' + infowindow.marker.getTitle() + '</div>'
     + '<div style="color:black">No further info available from FourSquare. Sorry :(');
 }
